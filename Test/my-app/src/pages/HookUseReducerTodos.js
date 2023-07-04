15053f09181f23a5f1f3ab47f23f545b9f8f330c
Taha Ansari <t.ansari@clerkswell.com>
2023-07-04 21:24:12 +0530

    wip

--- Test/my-app/src/pages/HookUseReducerTodos.js
+++ Test/my-app/src/pages/HookUseReducerTodos.js
@@ -1,17 +1,32 @@
-import { useState } from "react";
+// MAKE A REACT COMPONENT WHICH ACCEPT A TASK FROM A INPUT FIELD 
+// ADD THAT TASK AS TODO ARRAY AND PRINCE THAT ON THE SCREEN USING REDUCER CONCEPT
+
+import { useReducer, useState } from "react";
 
 function HookUseReducerTodos(){
 
-    const [task,addTask] = useState('')
+    const initialTodos =  { todos: [] }
 
-    const addTodo = () => {
-        console.log('add todo called');
-
+    const reducer = (state,action) =>{
+        switch(action.type){
+            case "addTodo":
+                return (todo)=>{ [...todo,action.payload.task] }
+        }
+        
     }
+
+    const [task,setTask] = useState('')
+
+    const [state,dispatch] = useReducer(reducer,initialTodos);
+
+    const addTodo = (task) => {
+        dispatch({type:"addTodo",payload:task})
+    }
+
     return(
         <>
-            <input type="text" value={task} onChange={  addTask((e)=>{e.target.valu3e})    } />
-            <button onClick={addTodo}>Add Todo</button>
+            <input type="text" value={task} onChange={(e)=>{setTask(e.target.value)}} />
+            <button onClick={()=>{addTodo(task)}}>Add Todo</button>
         </>
     )
 }
